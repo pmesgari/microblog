c6228e97919c967847b3248624e7eb48af089efb
Payam Mesgari <payam@techroom.nl>
2019-04-13 13:35:13 +0200

    database

--- app/__init__.py
+++ app/__init__.py
@@ -13,7 +13,13 @@ Points of interest:
     - `__name__` argument
 """
 from flask import Flask
+from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
-from app import routes
+from app import routes, models
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
 from app import app
-
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
@@ -17,3 +17,16 @@ def index():
         }
     ]
     return render_template('index.html', user=user, title='Home', posts=posts)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        msg = 'Login request for user {}, remember me={}'.format(
+            form.username.data,
+            form.remember_me.data
+        )
+        flash(msg)
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
